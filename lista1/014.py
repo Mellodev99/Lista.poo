cff253517af9d10c9b2e1ec21cee68203fe63ae1
Luan Melo dos santos. <luanmello350@gmail.com>
2026-08-28 23:18:36 -0300

    z

--- lista1/014.py
+++ lista1/014.py
@@ -1,1 +1,6 @@
-int(input('Digite '))
+produto = float(int('Digite o preco unitario do produto: '))
+quantidade = float(int('Digite a quantidade do produto: '))
+desconto = float(input('Digite o desconto a ser aplicado ao produto: '))
+
+valor_final = (produto * quantidade) - desconto
+print(f"valor finl da compra: {valor_final:.2f}")
